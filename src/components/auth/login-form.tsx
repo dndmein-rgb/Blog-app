aaac283d54313bfd2c937769990e84d8f8553aae
Divyanshu <dnd.mein@gmail.com>
2026-02-04 13:11:47 +0530

    Implement login form with validation using react-hook-form and zod

--- src/components/auth/login-form.tsx
+++ src/components/auth/login-form.tsx
@@ -1,6 +1,55 @@
+"use client"
+import { useState } from 'react'
+import { useForm } from 'react-hook-form'
+import {z} from 'zod'
+import { zodResolver } from "@hookform/resolvers/zod"
+import { Form, FormControl, FormField, FormItem, FormLabel } from '../ui/form'
+import { Input } from '../ui/input'
+const loginSchema=z.object({
+    email:z.string().email('Please enter a valid email address'),
+    password:z.string().min(6,"Password must be atleast 6 characters long")
+})
+type LoginFormvalues=z.infer<typeof loginSchema>
 function LoginForm() {
+    const [isLoading,setIsLoading]=useState(false)
+
+    //initialize form
+    const form=useForm<LoginFormvalues>({
+        resolver:zodResolver(loginSchema),
+        defaultValues:{
+            email:'',
+            password:''
+        }
+})
     return ( 
-        <div className="">Login Form</div>
+        <Form {...form} >
+            <form className='space-y-4'>
+            <FormField 
+                control={form.control}
+                name='email'
+                render={({field})=>(
+                    <FormItem>
+                        <FormLabel >Email</FormLabel>
+                        <FormControl>
+                            <Input placeholder='Enter your email' {...field} />
+                        </FormControl>
+                    </FormItem>
+                )}
+                 />
+            <FormField 
+                control={form.control}
+                name='password'
+                render={({field})=>(
+                    <FormItem>
+                        <FormLabel >Password</FormLabel>
+                        <FormControl>
+                            <Input type='password' placeholder='Enter your password' {...field} />
+                        </FormControl>
+                    </FormItem>
+                )}
+                 />
+            </form>
+        </Form>
      );
 }
 
