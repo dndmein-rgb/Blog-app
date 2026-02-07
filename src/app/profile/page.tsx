11c2fc01a53e10e4a6ddb5b4a4bb335b70718fd1
Divyanshu <dnd.mein@gmail.com>
2026-02-07 16:58:05 +0530

    feat: implement profile page with user authentication and account info display

--- src/app/profile/page.tsx
+++ src/app/profile/page.tsx
@@ -1,6 +1,52 @@
-function ProfilePage() {
+import { Button } from "@/components/ui/button";
+import { Card, CardContent, CardDescription, CardHeader, CardTitle } from "@/components/ui/card";
+import { auth } from "@/lib/auth";
+import { PlusCircle } from "lucide-react";
+import { headers } from "next/headers";
+import Link from "next/link";
+import { redirect } from "next/navigation";
+
+async function ProfilePage() {
+    const session =await auth.api.getSession({
+        headers:await headers()
+    })
+    if(!session || !session.user){
+        redirect('/')
+    }
     return ( 
-        <div className="">ProfilePage</div>
+        <main className="py-10">
+            <div className="max-w-7xl mx-auto px-4">
+                <div className="flex justify-between items-center mb-8">
+                    <div className="">
+                        <h1 className="text-3xl font-bold">Your Profile</h1>
+                    </div>
+                    <Button asChild>
+                        <Link href={'/post/create'}>
+                        <PlusCircle className="h-5 w-5 mr-2" />
+                        Create Post
+                        </Link>
+                    </Button>
+                </div>
+                <Card className="mb-8">
+                    <CardHeader>
+                        <CardTitle>
+                            Account Information
+                        </CardTitle>
+                        <CardDescription>Your Profile Info</CardDescription>
+                    </CardHeader>
+                    <CardContent>
+                        <div className="space-y-2">
+                            <div className="">
+                                <span className="font-medium">Name: </span>{session?.user.name}
+                            </div>
+                            <div className="">
+                                <span className="font-medium">Email: </span>{session?.user.email}
+                            </div>
+                        </div>
+                    </CardContent>
+                </Card>
+            </div>
+            </main>
      );
 }
 
